a9d6b1ec4ee8e198381754534607d79573ef1613
shapirolev2 <shapirolev2@gmail.com>
2023-03-28 09:41:43 +0300

    Add components 'CreateGame' which will appear as popup with input where to enter questions amount

---
+++ src/components/CreateGame/CreateGame.tsx
@@ -0,0 +1,71 @@
+import { FC } from "react";
+
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    TextField,
+} from "@mui/material";
+
+interface CreateGameProps {
+    open: boolean;
+    handleClose: () => void;
+
+    setQuestionsAmount: (amount: number) => void;
+}
+
+export const CreateGame: FC<CreateGameProps> = ({
+    open,
+    handleClose,
+    setQuestionsAmount,
+}) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setQuestionsAmount(data.amount);
+        handleClose();
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Create game</DialogTitle>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <DialogContent>
+                    <DialogContentText>
+                        To start the game you should enter amount of questions
+                        you would like to appear during the game
+                    </DialogContentText>
+                    <TextField
+                        {...register("amount", {
+                            required: true,
+                        })}
+                        error={!!errors.amount}
+                        helperText={
+                            errors?.amount?.type === "required" &&
+                            "This field is required"
+                        }
+                        autoFocus
+                        margin="dense"
+                        label="Amount of questions"
+                        fullWidth
+                        variant="outlined"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button type="submit" variant="contained">
+                        Submit
+                    </Button>
+                </DialogActions>
+            </form>
+        </Dialog>
+    );
+};
